30d82bd4ff52e625d138a920a8274053744f3b11
Misty <gyc990326@gmail.com>
2019-07-02 00:49:16 +0800

    Support windows

--- setup.bat
+++ setup.bat
@@ -16,8 +16,8 @@ if not exist cle git clone https://github.com/angr/cle.git || goto :error
 if not exist pyvex git clone https://github.com/angr/pyvex.git || goto :error
 if not exist vex git clone https://github.com/angr/vex.git || goto :error
 if not exist archinfo git clone https://github.com/angr/archinfo.git || goto :error
-if not exist angr-doc git clone https://github.com/angr/angr-doc.git || goto :error
-if not exist binaries git clone https://github.com/angr/binaries.git || goto :error
+Rem if not exist angr-doc git clone https://github.com/angr/angr-doc.git || goto :error
+Rem if not exist binaries git clone https://github.com/angr/binaries.git || goto :error
 if not exist wheels git clone https://github.com/angr/wheels.git || goto :error
 
 if not "%TO_CHECKOUT%" == "" (
